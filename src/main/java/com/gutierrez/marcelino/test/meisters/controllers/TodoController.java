1d12cf401d2db26eda4a0aa71168a6a3b1e1b0d2
guchierrez <marcelinogs2001@gmail.com>
2023-11-08 06:44:36 -0300

    add cross origin annotation

--- src/main/java/com/gutierrez/marcelino/test/meisters/controllers/TodoController.java
+++ src/main/java/com/gutierrez/marcelino/test/meisters/controllers/TodoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/todos")
 public class TodoController {
     @Autowired
