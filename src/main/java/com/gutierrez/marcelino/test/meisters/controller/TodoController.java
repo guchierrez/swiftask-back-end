b1c2b757d14cf3577316a38c7f4f248918988655
Marcelino Gutierrez <marcelino.soares@meisters.solutions>
2024-01-19 10:21:14 -0300

    specify allowed methods and origins to solve cors policy

--- src/main/java/com/gutierrez/marcelino/test/meisters/controller/TodoController.java
+++ src/main/java/com/gutierrez/marcelino/test/meisters/controller/TodoController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/todos")
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TodoController {
     private final TodoService todoService;
 
