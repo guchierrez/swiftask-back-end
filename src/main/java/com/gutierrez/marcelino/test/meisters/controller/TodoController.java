618132faad4e5bd4b592e3c3822b8cb434b309f8
Marcelino Gutierrez <marcelino.soares@meisters.solutions>
2024-01-19 10:14:25 -0300

    fix request mapping path

--- src/main/java/com/gutierrez/marcelino/test/meisters/controller/TodoController.java
+++ src/main/java/com/gutierrez/marcelino/test/meisters/controller/TodoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("todos")
+@RequestMapping("/todos")
 @CrossOrigin
 public class TodoController {
     private final TodoService todoService;
